7b7a19a53a81868fa2f88cf598a00a3b90f53109
PSepulvedaS <pablo.seplveda@uc.cl>
2024-08-11 17:58:01 -0400

    Fix xpath selectors and add sleep for stability

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from driver import Driver
+from time import sleep
 
 class Scrapper:
 
@@ -10,7 +11,9 @@ class Scrapper:
     def find_by_total_medals(self, quantity: int) -> None:
         self.chrome.click_element('//*[@id="onetrust-accept-btn-handler"]')
         self.chrome.click_element('//*[@id="__next"]/div/header/div/div[1]/nav[1]/nav[2]/a[3]')
-        self.chrome.click_element('//*[@id="p2024-main-content"]/div[1]/div[1]/div/div[1]/div[2]/div[1]/div[1]')  
+        self.chrome.click_element('//*[@id="p2024-main-content"]/div[1]/div[1]/div/div[1]/div[2]/div[1]/div[1]')
+        sleep(2)  
+        #/html/body/div[4]/div[1]/div[2]
         self.chrome.click_element('/html/body/div[4]/div[1]/div[2]')
         countries_info = []
 
@@ -18,16 +21,16 @@ class Scrapper:
 
         for index in range(1, quantity+1):
             country_info = []
-
-            country = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/div/span[3]')
-            gold = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[1]')
-            silver = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[2]')
-            bronce = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[3]')
-            total = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[4]')
+            #/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/span[3]
+            country = self.chrome.driver.find_element(f'/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/span[3]')
+            gold = self.chrome.driver.find_element(f'{xpath_father}div[{index}]/div/div/div/span[1]')
+            silver = self.chrome.driver.find_element(f'{xpath_father}div[{index}]/div/div/div/span[2]')
+            bronze = self.chrome.driver.find_element(f'{xpath_father}div[{index}]/div/div/div/span[3]')
+            total = self.chrome.driver.find_element(f'{xpath_father}div[{index}]/div/div/div/span[4]')
             country_info.append(country.text)
             country_info.append(gold.text)
             country_info.append(silver.text)
-            country_info.append(bronce.text)
+            country_info.append(bronze.text)
             country_info.append(total.text)
             countries_info.append(country_info)
         
@@ -41,17 +44,19 @@ class Scrapper:
         self.chrome.click_element('/html/body/div[4]/div[1]/div[3]')
         countries_info = []
 
-        for index in (1, quantity+1):
+        xpath_father = '/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/'
+
+        for index in range(1, quantity+1):
             country_info = []
-            country = self.chrome.find_element(f'/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[{index}]/div/div/div/span[3]')
-            gold = self.chrome.find_element(f'/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[{index}]/div/div/span[1]')
-            silver = self.chrome.find_element(f'/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[{index}]/div/div/span[2]')
-            bronce = self.chrome.find_element(f'/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[{index}]/div/div/span[3]')
-            total = self.chrome.find_element(f'/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[{index}]/div/div/span[4]')
+            country = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/div/span[3]')
+            gold = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[1]')
+            silver = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[2]')
+            bronze = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[3]')
+            total = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[4]')
             country_info.append(country.text)
             country_info.append(gold.text)
             country_info.append(silver.text)
-            country_info.append(bronce.text)
+            country_info.append(bronze.text)
             country_info.append(total.text)
             countries_info.append(country_info)
         
@@ -60,7 +65,7 @@ class Scrapper:
         
 
     def write_countries_csv(self, info: list, filename: str) -> None:
-        header = "PAIS;OROS;PLATAS;BRONCES;TOTAL\n"
+        header = "COUNTRY;GOLDS;SILVERS;BRONZES;TOTAL\n"
 
         with open(filename, mode='w', newline='', encoding='utf-8') as file:
             file.write(header)
