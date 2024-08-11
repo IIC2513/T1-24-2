c2d8571be87454aacb6fba36a5799f206a8c3b58
PSepulvedaS <pablo.seplveda@uc.cl>
2024-08-11 19:54:53 -0400

    fix: driver methods and unit tests

--- scrapper.py
+++ scrapper.py
@@ -11,43 +11,18 @@ class Scrapper:
     def find_by_total_medals(self, quantity: int) -> None:
         self.chrome.click_element('//*[@id="onetrust-accept-btn-handler"]')
         self.chrome.click_element('//*[@id="__next"]/div/header/div/div[1]/nav[1]/nav[2]/a[3]')
+        self.chrome.driver.execute_script("window.scrollTo(0, 100);")
         self.chrome.click_element('//*[@id="p2024-main-content"]/div[1]/div[1]/div/div[1]/div[2]/div[1]/div[1]')
-        sleep(2)  
-        #/html/body/div[4]/div[1]/div[2]
+        sleep(5)  
         self.chrome.click_element('/html/body/div[4]/div[1]/div[2]')
         countries_info = []
 
         xpath_father = '/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/'
+        self.chrome.driver.execute_script("window.scrollTo(0, 400);")
 
         for index in range(1, quantity+1):
             country_info = []
-            #/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/span[3]
-            country = self.chrome.driver.find_element(f'/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/span[3]')
-            gold = self.chrome.driver.find_element(f'{xpath_father}div[{index}]/div/div/div/span[1]')
-            silver = self.chrome.driver.find_element(f'{xpath_father}div[{index}]/div/div/div/span[2]')
-            bronze = self.chrome.driver.find_element(f'{xpath_father}div[{index}]/div/div/div/span[3]')
-            total = self.chrome.driver.find_element(f'{xpath_father}div[{index}]/div/div/div/span[4]')
-            country_info.append(country.text)
-            country_info.append(gold.text)
-            country_info.append(silver.text)
-            country_info.append(bronze.text)
-            country_info.append(total.text)
-            countries_info.append(country_info)
-        
-        self.write_countries_csv(countries_info, "total_medals.csv")
-
-
-    def find_by_alphabetical_order(self, quantity: int) -> None:
-        self.chrome.click_element('//*[@id="onetrust-accept-btn-handler"]')
-        self.chrome.click_element('//*[@id="__next"]/div/header/div/div[1]/nav[1]/nav[2]/a[3]')
-        self.chrome.click_element('//*[@id="p2024-main-content"]/div[1]/div[1]/div/div[1]/div[2]/div[1]/div[1]')  
-        self.chrome.click_element('/html/body/div[4]/div[1]/div[3]')
-        countries_info = []
-
-        xpath_father = '/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/'
-
-        for index in range(1, quantity+1):
-            country_info = []
+            sleep(5)
             country = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/div/span[3]')
             gold = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[1]')
             silver = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[2]')
@@ -60,7 +35,37 @@ class Scrapper:
             country_info.append(total.text)
             countries_info.append(country_info)
         
-        self.write_countries_csv(countries_info, "alphabetical_order.csv")
+        self.write_countries_csv(countries_info, "test/csv_student/total_medals.csv")
+
+
+    def find_by_alphabetical_order(self, quantity: int) -> None:
+        self.chrome.click_element('//*[@id="onetrust-accept-btn-handler"]')
+        self.chrome.click_element('//*[@id="__next"]/div/header/div/div[1]/nav[1]/nav[2]/a[3]')
+        self.chrome.driver.execute_script("window.scrollTo(0, 100);")
+        self.chrome.click_element('//*[@id="p2024-main-content"]/div[1]/div[1]/div/div[1]/div[2]/div[1]/div[1]')
+        sleep(5)  
+        self.chrome.click_element('/html/body/div[4]/div[1]/div[3]')
+        countries_info = []
+
+        xpath_father = '/html/body/div[1]/main/div[3]/div[1]/div[2]/div[2]/div/div[2]/'
+        self.chrome.driver.execute_script("window.scrollTo(0, 400);")
+
+        for index in range(1, quantity+1):
+            country_info = []
+            sleep(5)
+            country = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/div/span[3]')
+            gold = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[1]')
+            silver = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[2]')
+            bronze = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[3]')
+            total = self.chrome.find_element(f'{xpath_father}div[{index}]/div/div/div/span[4]')
+            country_info.append(country.text)
+            country_info.append(gold.text)
+            country_info.append(silver.text)
+            country_info.append(bronze.text)
+            country_info.append(total.text)
+            countries_info.append(country_info)
+        
+        self.write_countries_csv(countries_info, "test/csv_student/alphabetical_order.csv")
 
         
 
--- test/test.py
+++ test/test.py
@@ -1,1 +1,80 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
+
 import unittest
+import pandas as pd
+from scrapper import Scrapper
+from driver import Driver
+
+
+class ScraperTest(unittest.TestCase):
+
+    def setUp(self):
+        chrome = Driver()
+        chrome.initialize_driver()
+        chrome.load_page('https://olympics.com/')
+        self.scrapper = Scrapper(chrome)
+
+    
+    def test_find_top_medallists(self):
+
+        info = self.scrapper.find_top_medallists("United States of America",'Artistic Gymnastics', 3)
+        self.scrapper.write_medallists_csv(info, "top_medallists_country.csv")
+
+        base_content = pd.read_csv("test/csv_base/top_medallists_country.csv")
+        
+        try:
+            student_content = pd.read_csv("test/csv_student/top_medallists_country.csv")
+        except:
+            self.fail("No se creó el archivo top_medallists_country.csv")
+
+        self.assertTrue(base_content.columns.equals(student_content.columns), "Header incorrecto")
+        self.assertTrue(base_content.equals(student_content), "Los archivos no son iguales")   
+
+    def test_find_top_medallists_gender(self):
+        info = self.scrapper.find_top_medallists_gender("Female", 3)
+        self.scrapper.write_medallists_csv(info, "top_medallists_female.csv")
+
+        base_content = pd.read_csv("test/csv_base/top_medallists_female.csv") # ruta de los archivos solucion
+
+        try:
+            student_content = pd.read_csv("test/csv_student/top_medallists_female.csv") # ruta de los archivos estudiante
+        except:
+            self.fail("No se creó el archivo top_medallist_female.csv")
+
+        self.assertTrue(base_content.columns.equals(student_content.columns), "Header incorrecto")
+        self.assertTrue(base_content.equals(student_content), "Los archivos no son iguales")
+
+    def test_find_countries_by_total_medals(self):
+        self.scrapper.find_by_total_medals(3)
+
+        base_content = pd.read_csv("test/csv_base/total_medals.csv")
+
+        try:
+            student_content = pd.read_csv("test/csv_student/total_medals.csv")
+        except:
+            self.fail("No se creó el archivo total_medals.csv")
+        
+        self.assertTrue(base_content.columns.equals(student_content.columns), "Header incorrecto")
+        self.assertTrue(base_content.equals(student_content), "Los archivos no son iguales")
+    
+    def test_find_countries_by_alphabetical_order(self):
+        self.scrapper.find_by_alphabetical_order(3)
+
+        base_content = pd.read_csv("test/csv_base/alphabetical_order.csv")
+
+        try:
+            student_content = pd.read_csv("test/csv_student/alphabetical_order.csv")
+        except:
+            self.fail("No se creó el archivo alphabetical_order.csv")
+        
+        self.assertTrue(base_content.columns.equals(student_content.columns), "Header incorrecto")
+        self.assertTrue(base_content.equals(student_content), "Los archivos no son iguales")
+        
+
+if __name__ == '__main__':
+    unittest.main()
